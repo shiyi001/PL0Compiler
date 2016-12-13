698a5c068fe2ed57860494bb7a1b39a3d44ac0d0
shiyi <i_am_shiyi@163.com>
2016-12-13 21:39:42 +0800

    编写了Operator.java和SymType.java两个类，分别表示Pcode伪操作符和token类型

---
+++ src/Operator.java
@@ -0,0 +1,7 @@
+/**
+ *created by shiyi in 2016/12/13
+ *这是Pcode的伪操作符集合
+ */
+public enum Operator {
+	INT, CAL, LIT, LOD, STO, JMP, JPC, OPR;
+}
---
+++ src/SymType.java
@@ -0,0 +1,16 @@
+/**
+ *created by shiyi on 2016/12/13
+ *token类型
+ */
+public enum SymType {
+	BEG, END, IF, THEN, ELS, CON, PROC, VAR, DO, WHI, CAL, REA, WRI, ODD,
+	//begin, end, if, then, else, const, var, do, while, call, read, write, odd
+	EQU, LES, LESE, LARE, LAR, NEQE, ADD, SUB, MUL, DIV, 
+	//=, <, <=, >=, >, <>, +, -, *, /
+	SYM, CONST,
+	//标识符， 常量
+	CEQU, COMMA, SEMIC, POI, LBR, RBR,
+	//:=, ',' , ';', '.', '(', ')'
+	COL;
+	//:
+}
