c7dc4745fc00e8cf4838e1f63941b95d8a38e3fe
shiyi <i_am_shiyi@163.com>
2016-12-14 20:38:10 +0800

    完成了符号表相关的类，但是功能还不够完善，需要在实现语法和语义分析时按具体需求再添加。

---
+++ src/AllSymbol.java
@@ -0,0 +1,46 @@
+/**
+ * created by shiyi on 2016/12/14
+ * 符号表
+ */
+public class AllSymbol {
+	List<PerSymbol> allSymbol;
+
+    private int con=1;              //常量类型用1表示
+    private int var=2;              //变量类型用2表示
+    private int proc=3;             //过程类型用3表示
+
+	public AllSymbol() {
+		allSymbol = new AllSymbol();
+	}
+
+	//向符号表中插入常量
+	public void enterConst(String name, int level, int value, int address) {
+        allSymbol.add(new PerSymbol(con, value, level, address, 4, name));
+    }
+
+    //向符号表中插入变量
+    public void enterVar(String name, int level, int address) {
+    	allSymbol.add(new PerSymbol(var, level, address, 4, name));
+    }
+
+    //向符号表中插入过程
+    public void enterProc(String name, int level, int address) {
+    	allSymbol.add(new PerSymbol(proc, level, address, 4, name));
+    }
+
+	public int getLength() {
+		return allSymbol.size();
+	}
+
+	public int getCon() {
+		return con;
+	}
+
+	public int getVar() {
+		return var;
+	}
+
+	public int getProc() {
+		return proc;
+	}
+}
--- src/PerSymbol.java
+++ src/PerSymbol.java
@@ -1,4 +1,78 @@
-
+/**
+ * created by shiyi on 2016/12/14
+ * PerSymbol类
+ */
 public class PerSymbol {
+	private int type;           //表示常量、变量或过程
+    private int value;          //表示常量或变量的值
+    private int level;          //嵌套层次
+    private int address;        //相对于所在嵌套过程基地址的地址
+    private int size;           //表示常量，变量，过程所占的大小
+    private String name;        //变量、常量或过程名
 
+    public PerSymbol(int _type, int _value, int _level, int _address, int _size, String _name) {
+    	type = _type;
+    	value = _value;
+    	level = _level;
+    	address = _address;
+    	size = _size;
+    	name = _name;
+    }
+
+    public PerSymbol(int _type, int _level, int _address, int _size, String _name) {
+    	//变量刚声明时没有初始值，过程声明没有值
+    	type = _type;
+    	level = _level;
+    	address = _address;
+    	size = _size;
+    	name = _name;
+    }
+
+    public void setType(int _type) {
+    	type = _type;
+    }
+
+    public void setValue(int _value) {
+        value = _value;
+    }
+
+    public void setLevel(int _level) {
+        level = _level;
+    }
+
+    public void setAddress(int _address) {
+        address = _address;
+    }
+
+    public void setSize(int _size) {
+        size = _size;
+    }
+
+    public void setName(String _name) {
+        name = _name;
+    }
+
+    public int  getType() {
+        return type;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public int getAddress() {
+        return address;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
